24b52cddfd4dadea2eabb8dba76876c4d34c37ab
Agung Firmansyah <agungfir98@gmail.com>
2024-03-25 20:31:03 +0700

    feat: google oauth sign in with supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:oraboros/screens/auth.dart';
 import 'package:oraboros/screens/dashboard.dart';
+import 'package:oraboros/screens/splash_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://cqixzlruhkitdqgwalvx.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxaXh6bHJ1aGtpdGRxZ3dhbHZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDEzMzUzMDMsImV4cCI6MjAxNjkxMTMwM30.lTTPfYHxRPDzMrJaGBhAXhpqQln05YxkzpDdiS6G6A4',
+  );
+
   runApp(const MyApp());
 }
+
+// It's handy to then extract the Supabase client in a variable for later uses
+final supabase = Supabase.instance.client;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,7 +26,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        "/": (context) => const MyHomePage(),
+        "/": (context) => const SplashPage(),
+        "/dashboard": (context) => const MyHomePage(),
         "/auth": (context) => const Authpage()
       },
       title: 'Oraboros',
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:oraboros/main.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    _redirect();
+  }
+
+  Future<void> _redirect() async {
+    await Future.delayed(Duration.zero);
+    if (!mounted) {
+      return;
+    }
+
+    final session = supabase.auth.currentSession;
+    if (session != null) {
+      Navigator.of(context).pushReplacementNamed('/dashboard');
+    } else {
+      Navigator.of(context).pushReplacementNamed('/auth');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(),
+              Text(
+                "ASUI",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
