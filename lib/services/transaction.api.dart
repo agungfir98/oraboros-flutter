bd5435c61a0217ec0dd1e2e149f0cbaadafe7196
Agung Firmansyah <agungfir98@gmail.com>
2024-04-23 16:43:20 +0700

    refactor: change directory name from fetcher to services for better context

---
+++ lib/services/transaction.api.dart
@@ -0,0 +1,21 @@
+import 'package:oraboros/main.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class TransactionService {
+  Future newTransaction(String userId, List<Map<String, dynamic>> data) async {
+    try {
+      PostgrestMap transaction = await supabase
+          .from('transactions')
+          .insert({"user_id": userId})
+          .select()
+          .single();
+
+      List<Map<String, dynamic>> orderList = data.map((order) {
+        return {...order, 'transaction_id': transaction['id']};
+      }).toList();
+      supabase.from('orders').insert(orderList).then((value) {});
+    } catch (e) {
+      throw Exception('something wrong with the server');
+    }
+  }
+}
