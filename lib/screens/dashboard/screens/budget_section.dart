98c0bcbc77f94579ac5bfa78126edc55f31ae4f5
Agung Firmansyah <agungfir98@gmail.com>
2024-04-23 16:44:19 +0700

    feat: budget list section with swipe reload indicator

--- lib/screens/dashboard/screens/budget_section.dart
+++ lib/screens/dashboard/screens/budget_section.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:oraboros/DTO/budget.dto.dart';
-import 'package:oraboros/fetcher/budget.api.dart';
+import 'package:oraboros/services/budget.service.dart';
 import 'package:oraboros/providers/profile.provider.dart';
 import 'package:provider/provider.dart';
 
@@ -47,17 +47,34 @@ class _BudgetSectionState extends State<BudgetSection> {
           style: Theme.of(context).textTheme.bodyMedium,
         ),
       ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            ListView.separated(
+      body: RefreshIndicator(
+        color: const Color(0xff122334),
+        onRefresh: fetchData,
+        child: SafeArea(
+          child: Container(
+            margin: const EdgeInsets.only(bottom: 40),
+            child: ListView.separated(
+              padding: const EdgeInsets.symmetric(vertical: 10),
               shrinkWrap: true,
               itemCount: _budgetList.length,
-              physics: const ClampingScrollPhysics(),
+              physics: const ClampingScrollPhysics(
+                parent: AlwaysScrollableScrollPhysics(),
+              ),
               itemBuilder: (context, index) {
                 return Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
                   decoration: BoxDecoration(
                     border: Border.all(width: 1),
+                    color: Colors.white,
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Color(0xff122334),
+                        offset: Offset(3, 5),
+                      ),
+                    ],
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(10),
+                    ),
                   ),
                   child: ListTile(
                     style: ListTileStyle.list,
@@ -81,8 +98,8 @@ class _BudgetSectionState extends State<BudgetSection> {
                   const SizedBox(
                 height: 10,
               ),
-            )
-          ],
+            ),
+          ),
         ),
       ),
     );
