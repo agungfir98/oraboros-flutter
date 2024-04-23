ae5ec452cc245bead087172f63d69338f0c415b1
Agung Firmansyah <agungfir98@gmail.com>
2024-04-23 16:48:03 +0700

    build: changing minimum sdk version to 21 for emojiPicker support

--- lib/screens/dashboard/screens/transaction_section.dart
+++ lib/screens/dashboard/screens/transaction_section.dart
@@ -1,14 +1,136 @@
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
+import 'package:flutter/foundation.dart' as foundation;
 import 'package:flutter/material.dart';
 
-class TransactionSection extends StatelessWidget {
+/// Example for EmojiPicker
+class TransactionSection extends StatefulWidget {
   const TransactionSection({super.key});
 
   @override
+  State<TransactionSection> createState() => _TransactionSection();
+}
+
+class _TransactionSection extends State<TransactionSection> {
+  final _controller = TextEditingController();
+  final _scrollController = ScrollController();
+  bool _emojiShowing = false;
+
+  @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        Text("transactions"),
-      ],
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            title: const Text('Emoji Picker Example App'),
+          ),
+          body: Column(
+            children: [
+              Expanded(
+                child: Center(
+                  child: ValueListenableBuilder(
+                    valueListenable: _controller,
+                    builder: (context, text, child) {
+                      return Text(
+                        _controller.text,
+                      );
+                    },
+                  ),
+                ),
+              ),
+              Container(
+                  height: 66.0,
+                  color: Colors.blue,
+                  child: Row(
+                    children: [
+                      Material(
+                        color: Colors.transparent,
+                        child: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _emojiShowing = !_emojiShowing;
+                            });
+                          },
+                          icon: const Icon(
+                            Icons.emoji_emotions,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                          child: TextField(
+                              controller: _controller,
+                              scrollController: _scrollController,
+                              style: const TextStyle(
+                                fontSize: 20.0,
+                                color: Colors.black87,
+                              ),
+                              maxLines: 1,
+                              decoration: InputDecoration(
+                                hintText: 'Type a message',
+                                filled: true,
+                                fillColor: Colors.white,
+                                contentPadding: const EdgeInsets.only(
+                                  left: 16.0,
+                                  bottom: 8.0,
+                                  top: 8.0,
+                                  right: 16.0,
+                                ),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(50.0),
+                                ),
+                              )),
+                        ),
+                      ),
+                      Material(
+                        color: const Color.fromARGB(0, 219, 83, 83),
+                        child: IconButton(
+                            onPressed: () {
+                              // send message
+                            },
+                            icon: const Icon(
+                              Icons.send,
+                              color: Colors.white,
+                            )),
+                      )
+                    ],
+                  )),
+              Offstage(
+                offstage: !_emojiShowing,
+                child: EmojiPicker(
+                  textEditingController: _controller,
+                  scrollController: _scrollController,
+                  config: Config(
+                    height: 256,
+                    checkPlatformCompatibility: true,
+                    emojiViewConfig: EmojiViewConfig(
+                      // Issue: https://github.com/flutter/flutter/issues/28894
+                      emojiSizeMax: 28 *
+                          (foundation.defaultTargetPlatform ==
+                                  TargetPlatform.iOS
+                              ? 1.2
+                              : 1.0),
+                    ),
+                    swapCategoryAndBottomBar: false,
+                    skinToneConfig: const SkinToneConfig(),
+                    categoryViewConfig: const CategoryViewConfig(),
+                    bottomActionBarConfig: const BottomActionBarConfig(),
+                    searchViewConfig: const SearchViewConfig(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
