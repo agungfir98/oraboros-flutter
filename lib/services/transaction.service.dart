6166bca835c83c654cb1438d3938e165c130a01d
Agung Firmansyah <agungfir98@gmail.com>
2024-04-29 19:55:33 +0700

    feat: getUserTransaction service

--- lib/services/transaction.service.dart
+++ lib/services/transaction.service.dart
@@ -1,8 +1,11 @@
+import 'package:oraboros/DTO/order.dto.dart';
 import 'package:oraboros/DTO/transaction.dto.dart';
 import 'package:oraboros/main.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class TransactionService {
+  final String table = "transactions";
+
   Future newTransaction(
       String userId, List<TransactionDTO> transactionData) async {
     List<Map<String, dynamic>> data =
@@ -10,7 +13,7 @@ class TransactionService {
 
     try {
       PostgrestMap transaction = await supabase
-          .from('transactions')
+          .from(table)
           .insert({"user_id": userId})
           .select()
           .single();
@@ -22,5 +25,34 @@ class TransactionService {
     } catch (e) {
       throw Exception('something wrong with the server');
     }
+  }
+
+  Future<List<UserTransactions>> getUserTransaction(String userId) async {
+    List<dynamic> data = await supabase
+        .from(table)
+        .select('*, orders(*,budgets(icon))')
+        .eq('user_id', userId);
+
+    return data.map(
+      (item) {
+        List<UserOrderDTO> orderList = [];
+
+        for (var item in item[UserTransactions.ordersKey]) {
+          if (item is Map<String, dynamic>) {
+            UserOrderDTO order = UserOrderDTO.fromJson(item);
+            orderList.add(order);
+          } else {
+            print("Unexpected data type in list: ${item.runtimeType}");
+          }
+        }
+
+        return UserTransactions(
+          id: item[UserTransactions.idKey],
+          createdAt: item[UserTransactions.createdAtKey],
+          orders: orderList,
+          userId: item[UserTransactions.userIdKey],
+        );
+      },
+    ).toList();
   }
 }
